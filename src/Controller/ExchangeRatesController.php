fd44a79b54676d2f1b1c66df6e2bbbe1876b0a14
ilkin <i.fleydanli@gmail.com>
2020-05-26 11:58:59 +0300

    Changes in logic, added groupBy method to group by datetime

--- src/Controller/ExchangeRatesController.php
+++ src/Controller/ExchangeRatesController.php
@@ -6,6 +6,7 @@ use App\Entity\ExchangeRates;
 use App\Repository\ExchangeRatesRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,15 +16,59 @@ class ExchangeRatesController extends AbstractController
 {
 
     /**
-     * @Route("/exchange/rates", name="exchange_rates" )
+     * @Route("/api/rates/{from?}/{to?}", name="api_rates" )
+     * @param  Request  $request
+     * @return JsonResponse
      */
-    public function index()
+    public function index(Request $request)
     {
-        $exchangeRates = $this->getDoctrine()->getRepository(ExchangeRates::class);
-        die(var_dump($exchangeRates->find(1)));
-        return new JsonResponse($data, Response::HTTP_OK);
-        return $this->render('exchange_rates/index.html.twig', [
-            'controller_name' => 'ExchangeRatesController',
-        ]);
+        // use this to get all the available attributes (not only routing ones):
+        $allAttributes = $request->attributes->all();
+        $from = $request->attributes->get('from');
+        $to = $request->attributes->get('from');
+        if ( $from || $to ) {
+            $exchangeRates = $this->getDoctrine()->getRepository(ExchangeRates::class)->findByDateRange(
+                $from,
+                $to
+            );
+        } else {
+            $exchangeRates = $this->getDoctrine()->getRepository(ExchangeRates::class)->findAll();
+        }
+
+        $newData = $this->groupBy("datetime", $exchangeRates);
+
+        return $this->json($newData, Response::HTTP_OK);
+
+    }
+
+    /**
+     * Function that groups an array of associative arrays by some key.
+     *
+     * @param  string  $key
+     * @param  array  $exchangeRates
+     * @return array
+     */
+    private function groupBy(string $key = 'datetime', array $exchangeRates = []): array {
+        $result = array();
+        $data = [];
+
+        foreach ($exchangeRates as $exchangeRate) {
+            $data[] = [
+                'id' => $exchangeRate->getId(),
+                'code' => $exchangeRate->getCode(),
+                'value' => $exchangeRate->getValue(),
+                'datetime' => (string)$exchangeRate->getDateTime()->format('Y-m-d H:i:s'),
+            ];
+        }
+
+        foreach($data as $val) {
+            if(array_key_exists($key, $val)){
+                $result[$val[$key]][] = $val;
+            }else{
+                $result[""][] = $val;
+            }
+        }
+
+        return $result;
     }
 }
